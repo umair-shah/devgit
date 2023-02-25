b6e92e2f8555a2f57de820edce2b0df33c007255
umair shah <umairshah098123@gmail.com>
2023-02-26 02:09:55 +0500

    your hello

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,6 +31,7 @@ namespace new_project.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            Console.writeline("hell0");
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
